9dd37232803c0660c9cc4f42a12eabc564464a0d
Nick Fredricks <nick.fredricks@flypaper.com>
2021-04-26 17:47:54 -0400

    initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            LoggerConfiguration loggerConfig = new LoggerConfiguration().MinimumLevel.Information();
+
+
+            loggerConfig.WriteTo.Sentry(o =>
+            {
+                o.Dsn = "YOUR_DSN_HERE";
+                o.Debug = true;
+                o.Environment = "test";
+            });
+
+            Log.Logger = loggerConfig.CreateLogger();
+
+            Log.Logger.Error("error");
+        }
+           
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Use the file name to load the assembly into the current
+            // application domain.
+            //Assembly a = Assembly.LoadFrom(Path.Combine("../../../", "ClassLibrary1", "bin","Debug","ClassLibrary1.dll"));
+            Assembly a = Assembly.Load("ClassLibrary1");
+
+            // Get the type to use.
+            Type myType = a.GetType("ClassLibrary1.Class1");
+            // Get the method to call.
+            // MethodInfo myMethod = myType.GetMethod("MethodA");
+            // Create an instance.
+            object obj = Activator.CreateInstance(myType);
+            // Execute the method.
+            // myMethod.Invoke(obj, null);
+        }
+    }
+}
